6aa35b2cb5a8c0de32d38ddca9b37f67de60dd2b
이해은 <haeeun47@gmail.com>
2020-12-26 20:13:36 +0900

    [ADD] 파일명 정렬

---
+++ 파일명 정렬.js
@@ -0,0 +1,31 @@
+function solution(files) {
+
+    files.sort((a,b) => {
+        let objectA = slice(a);
+        let objectB = slice(b);
+
+        return objectA.head < objectB.head ?
+            -1 : objectA.head > objectB.head ?
+                1 : objectA.number - objectB.number
+    })
+
+    return files;
+}
+
+function slice(string){
+    let Object = {
+        head: '',
+        number: '',
+    };
+    string.split('');
+    for(let i=0; i<string.length; i++){
+        if( (isNaN(string[i]) || string[i] === ' ') && Object.number === ''){
+            Object.head += string[i].toLowerCase();
+        } else if(!isNaN(string[i])){
+            Object.number += string[i];
+        } else {
+            break;
+        }
+    }
+    return Object
+}
